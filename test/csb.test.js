039623e704b904fdacd6993aab5a9a66b9a937a5
Fumikazu Fujiwara <github@ze.gs>
2017-08-13 09:07:14 +0900

    add csb.js

---
+++ test/csb.test.js
@@ -0,0 +1,9 @@
+/* eslint no-invalid-this: 0 */
+import chai from 'chai';
+chai.should();
+
+describe('CSB test.', (suite) => {
+  it('dummy test', () => {
+      "".should.be.a('string');
+  });
+});
